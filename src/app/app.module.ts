48b5ceb09aae2c06e928893c01ba59702c4d2d67
inés <icarazo@cic.es>
2018-05-15 09:36:14 +0200

    Añadido login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,47 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { LoggedinPage } from '../pages/loggedin/loggedin';
+import { RegisterPage } from './../pages/register/register';
+
+const firebaseAuth = {
+  apiKey: "AIzaSyDecPLTcBODdn8fU9hUd9VFymsBZjSFaqk",
+  authDomain: "proyectogit-53439.firebaseapp.com",
+  databaseURL: "https://proyectogit-53439.firebaseio.com",
+  projectId: "proyectogit-53439",
+  storageBucket: "",
+  messagingSenderId: "746709652335"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage
   ],
   providers: [
     StatusBar,
